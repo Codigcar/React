4640c5ad7254c3e1c204ec778f8747897a80adc4
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-02-21 19:16:02 -0500

    03 - Evento click

--- 03-primeros-pasos/src/CounterApp.js
+++ 03-primeros-pasos/src/CounterApp.js
@@ -4,10 +4,16 @@ import PropTypes from 'prop-types';
 // functional components
 
 const CounterApp= ( numero ) => {
+
+    const handleAdd = (e) => {
+        console.log(e);
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{numero.numero}</h2>
+            <button onClick={ e => handleAdd(e) }>+1</button>
         </>
     );
 }
