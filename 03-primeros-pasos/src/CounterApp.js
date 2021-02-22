baf2024f46fbb0c3e917f2786d526c10841af3b3
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-02-21 19:39:52 -0500

    03 - useState_Hook

--- 03-primeros-pasos/src/CounterApp.js
+++ 03-primeros-pasos/src/CounterApp.js
@@ -1,18 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 // functional components
 
 const CounterApp= ( numero ) => {
 
-    const handleAdd = (e) => {
-        console.log(e);
+    /* const state = useState('Goku');
+    console.log(state); */
+   
+    /*  const [nombre, setNombre] = useState('Goku');
+    console.log(nombre);
+    console.log(setNombre); */
+
+    const [counter, setCounter] = useState(10); // retorna un arreglo con 2 valores
+
+
+    const handleAdd = () => {
+         setCounter( counter + 1);
+        // setCounter( c => c + 1);
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{numero.numero}</h2>
+            <h2>{counter}</h2>
             <button onClick={ e => handleAdd(e) }>+1</button>
         </>
     );
