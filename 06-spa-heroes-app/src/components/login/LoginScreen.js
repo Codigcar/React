787c768201f750db29f034ef01098f92c9c6876c
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-07 16:13:51 -0500

    06 - History push replace - Ir a la otra pag y poder regresar dependiendio

--- 06-spa-heroes-app/src/components/login/LoginScreen.js
+++ 06-spa-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+
+    const handleClick = () => {
+        // Puede regresar a la pagina anterior
+        history.push('/');
+        // No se puede regresar a la pagina anterior porque lo elimina
+        // history.replace('/');
+    }
+
     return (
-        <div>
+        <div className="container mt-5">
             <h1>LoginScreen</h1>
+            <hr />
+
+            <button className="btn btn-primary" onClick={ handleClick }>
+                Login
+            </button>
         </div>
     )
 }
