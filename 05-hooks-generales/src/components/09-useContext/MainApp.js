c5ce0aa1059d371f2d5d96850b1f34ebc365b551
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-06 22:14:36 -0500

    05 - useContext - CreateContext y useContext - Pasar información del User

--- 05-hooks-generales/src/components/09-useContext/MainApp.js
+++ 05-hooks-generales/src/components/09-useContext/MainApp.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
 import { HomeScreen } from './HomeScreen'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+
+    const user = {
+        id: 1,
+        name: 'Carlos',
+        email: 'carlos@gmail.com'
+    }
+
     return (
+        <UserContext.Provider value={ user }>
             <AppRouter />
+        </UserContext.Provider>
     )
 }
