6b420dfe51a42f9024132b6a6f233029da23d807
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-08 00:13:57 -0500

    06 - Leer argumentos por URL - get Id del URL y mostrar

--- 06-spa-heroes-app/src/components/heroes/HeroScreen.js
+++ 06-spa-heroes-app/src/components/heroes/HeroScreen.js
@@ -1,6 +1,28 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import { getHeroesById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+    const { heroeId } = useParams();
+    console.log(heroeId);
+
+    const hero = getHeroesById(heroeId);
+    console.log(hero);
+    
+    if(!hero) {
+        return <Redirect to="/" />;
+    }
+
+    const {
+        id,
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero
+
     return (
         <div>
             <h1>HeroScreen</h1>
