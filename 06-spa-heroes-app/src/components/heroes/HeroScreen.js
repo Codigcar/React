b22e3aaa5f29d88018c36762071c4db287866164
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-09 15:08:13 -0500

    06 - Mostra info del heroe especifico y regresar a la pag anterior

--- 06-spa-heroes-app/src/components/heroes/HeroScreen.js
+++ 06-spa-heroes-app/src/components/heroes/HeroScreen.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { Redirect, useParams } from 'react-router'
 import { getHeroesById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
 
     const { heroeId } = useParams();
     console.log(heroeId);
 
     const hero = getHeroesById(heroeId);
-    console.log(hero);
     
     if(!hero) {
         return <Redirect to="/" />;
@@ -23,9 +22,41 @@ export const HeroScreen = () => {
         characters
     } = hero
 
+    const handleReturn = () => {
+
+        // Validando si ingresó a un heroe especifico de forma directa mediante url
+        if(history.length <= 2){
+            history.push('/');
+        }
+
+        history.goBack();
+    }
+
     return (
-        <div>
-            <h1>HeroScreen</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img src={ `../assets/heroes/${ heroeId }.jpg`} className="img-thumbnail" alt={ superhero } />
+            </div>
+
+            <div className="col-8">
+                <h3>{ superhero }</h3>
+
+                <ul className="list-group list-group-flush">
+                    <li className="list-grou-item"> <b> Alter ego: </b> { alter_ego } </li>
+                    <li className="list-grou-item"> <b> Publisher: </b> { publisher } </li>
+                    <li className="list-grou-item"> <b> First appearance: </b> { first_appearance } </li>
+                </ul>
+
+                <h5> Character </h5>
+                <p> {characters} </p>
+
+                <button className="btn btn-outline-info" onClick={ handleReturn }>
+                    Return
+                </button>
+
+
+            </div>
+            
         </div>
     )
 }
