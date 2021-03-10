2faee9ac8652743c240cb245c77e14e4fb399299
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-10 13:28:52 -0500

    07 - Logout - Volver a /login con dependencia {history} del DashBoard

--- 07-spa-rutas-protegidas/src/components/ui/Navbar.js
+++ 07-spa-rutas-protegidas/src/components/ui/Navbar.js
@@ -1,12 +1,20 @@
 import React, { useContext } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
-export const Navbar = () => {
+export const Navbar = ({  history }) => {
 
 
-    const { user:{ name } } = useContext( AuthContext );
+    const { user:{ name }, dispatch } = useContext( AuthContext );
     // console.log(name);
+
+    const handleLogout = () => {
+        history.replace('/login');
+        dispatch({
+            type: types.logout
+        })
+    }
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -59,14 +67,12 @@ export const Navbar = () => {
                     </span>
 
 
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
+                    <button 
+                        className="nav-item nav-link btn" 
+                        onClick={ handleLogout }
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
