b6295f4553b4f0ece506261d6d6fa779f63b6977
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-12 16:35:53 -0500

    08 - Estilo Estructura y diseño del HomeScreen - Coloca titulo y textArea e img

--- 08-journalApp/src/components/notes/NoteScreen.js
+++ 08-journalApp/src/components/notes/NoteScreen.js
@@ -5,6 +5,15 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+
+            <div className="notes__content">
+                <input className="notes__title-input" type="text" placeholder="Some awsome title" />
+                <textarea className="notes__textarea" placeholder="What happend today">                </textarea>
+                <div className="notes__img">
+                    <img src="https://as.com/meristation/imagenes/2019/08/05/noticias/1565010078_514445_1565010136_noticia_normal.jpg" alt="img" />
+                </div>
+            </div>
+
         </div>
     )
 }
