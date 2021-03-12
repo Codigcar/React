a5dbf7b1e7b7ca6552036f5745b78c30fa92cce9
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-11 19:00:59 -0500

    08 - inicio e instalado sass css
    npm install sass

---
+++ 08-journalApp/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+import { JournalApp } from './JournalApp';
+// instalar: npm install node-sass
+import './styles/style.scss';
+
+ReactDOM.render(
+  <JournalApp />,
+  document.getElementById('root')
+);
+
