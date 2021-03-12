74541bab58012c25c9a9e194baf3c726dd3d956f
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-12 16:10:35 -0500

    08 - Estilo JournalScreen > NoteScreen > NotesAppBar

---
+++ 08-journalApp/src/components/notes/NotesAppBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NotesAppBar = () => {
+    return (
+        <div className="notes_appbar">
+            <span>30 agosto 2021</span>
+
+            <div>
+                <button className="btn">Picture</button>
+                <button className="btn">Save</button>
+            </div>
+        </div>
+    )
+}
