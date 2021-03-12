e7deb1c035dcc5b5945ea7e8931cee3729959aec
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-12 01:12:55 -0500

    08 - Estilos dele AuthRouter y html al LoginScreen

--- 08-journalApp/src/components/auth/LoginScreen.js
+++ 08-journalApp/src/components/auth/LoginScreen.js
@@ -4,6 +4,14 @@ export const LoginScreen = () => {
     return (
         <div>
             <h1>LoginScreen</h1>
+
+            <form>
+                <input type="text" placeholder="email" name="email" />
+                <input type="password" placeholder="Password" name="password" />
+                <button type="submit">Login</button>
+                <hr />
+                google
+            </form>
         </div>
     )
 }
