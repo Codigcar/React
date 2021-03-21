932fc2767fce2804faee0bbda0e33f06835c9130
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-21 18:25:46 -0500

    09 - Manejor errores del Form +  valid, errores show in consola

--- 09-journalApp-redux/src/components/auth/RegisterScreen.js
+++ 09-journalApp-redux/src/components/auth/RegisterScreen.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm'
 
+// npm i validator   
+import validator from 'validator';
+
+
 export const RegisterScreen = () => {
 
     const [formValues, handleInputChange ] = useForm({
@@ -15,7 +19,27 @@ export const RegisterScreen = () => {
 
     const handleRegister = (e) => {
         e.preventDefault();
-        console.log(name, email, password, password2);
+        
+        if( isFormValid() ) {
+            console.log(name, email, password, password2);
+        } 
+
+    }
+
+    const isFormValid = () => {
+        if( name.trim().length === 0) {
+            console.log('Name is required');
+            return false;
+        } else if ( !validator.isEmail( email )) {
+            // npm i validator 
+            console.log('Email is not valid');
+            return false;
+        } else if ( password !== password2 || password.length < 5  ){
+            console.log('Password should be at least 6 characters and match each other');
+            return false;
+        }
+        return true;
+
     }
 
 
@@ -24,6 +48,9 @@ export const RegisterScreen = () => {
             <h3 className="auth__title">Register</h3>
 
             <form onSubmit={ handleRegister }>
+
+                <div className="auth__alert-error">Error</div>
+
                 <input    value={ name }        onChange={ handleInputChange } name="name"             className="auth__input" type="text" placeholder="name"  autoComplete="off" />
                 <input    value={ email }       onChange={ handleInputChange } name="email"            className="auth__input" type="text" placeholder="email"  autoComplete="off" />
                 <input    value={ password }    onChange={ handleInputChange } name="password"         className="auth__input" type="password" placeholder="password"  />
