195268a70dcbfa18275c0c8d4bc9d77305d3722b
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-03-21 18:12:49 -0500

    09 - RegisterScreen imprimir data en consola al presion btn-enviar

--- 09-journalApp-redux/src/components/auth/RegisterScreen.js
+++ 09-journalApp-redux/src/components/auth/RegisterScreen.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
 
 export const RegisterScreen = () => {
+
+    const [formValues, handleInputChange ] = useForm({
+        name:'',
+        email:'',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        console.log(name, email, password, password2);
+    }
+
+
     return (
         <>
             <h3 className="auth__title">Register</h3>
 
-            <form>
-                <input className="auth__input" type="text" placeholder="name" name="name" autoComplete="off" />
-                <input className="auth__input" type="text" placeholder="email" name="email" autoComplete="off" />
-                <input className="auth__input" type="password" placeholder="password" name="password" />
-                <input className="auth__input" type="password" placeholder="confirm password" name="password2" />
+            <form onSubmit={ handleRegister }>
+                <input    value={ name }        onChange={ handleInputChange } name="name"             className="auth__input" type="text" placeholder="name"  autoComplete="off" />
+                <input    value={ email }       onChange={ handleInputChange } name="email"            className="auth__input" type="text" placeholder="email"  autoComplete="off" />
+                <input    value={ password }    onChange={ handleInputChange } name="password"         className="auth__input" type="password" placeholder="password"  />
+                <input    value={ password2 }   onChange={ handleInputChange } name="password2"        className="auth__input" type="password" placeholder="confirm password"  />
 
                 <button type="submit" className="btn btn-primary btn-block mb-5"> Register </button>
 
